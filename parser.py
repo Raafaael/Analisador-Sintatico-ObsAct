fe0e747fdd8d694a6ee2df04a4529343a1eebbf8
Dante <dantenavaza2005@gmail.com>
2025-06-30 21:11:07 -0300

    tirar log

--- parser.py
+++ parser.py
@@ -1,3 +1,7 @@
+'''
+VALIDAR KEY WORDS
+'''
+
 import ply.lex as lex
 import ply.yacc as yacc
 
--- runtime.py
+++ runtime.py
@@ -1,19 +1,11 @@
 def ligar(device):
     print(f"{device} ligado!")
-    log(f"{device} ligado!")
 
 def desligar(device):
     print(f"{device} desligado!")
-    log(f"{device} desligado!")
 
 def alerta(device, msg):
     print(f"{device} recebeu o alerta:\n{msg}")
-    log(f"{device} recebeu o alerta: {msg}")
 
 def alerta_var(device, msg, var):
     print(f"{device} recebeu o alerta:\n{msg} {var}")
-    log(f"{device} recebeu o alerta: {msg} {var}")
-
-def log(message):
-    with open('log.txt', 'a', encoding='utf-8') as f:
-        f.write(message + '\n')
